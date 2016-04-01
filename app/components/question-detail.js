3fd76a315bd55ab4dde835c1a376d80ce3cc96da
chris berry <chrisberry321@yahoo.com>
2016-04-01 14:58:21 -0700

    start of dynamic routing, only displaying question and name on index

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isNotesShowing: false,
   actions: {
+    notesShow: function() {
+      this.set('isNotesShowing', true);
+    },
+
+    notesHide: function() {
+      this.set('isNotesShowing', false);
+    },
+
     delete(question) {
       if (confirm('Why you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
