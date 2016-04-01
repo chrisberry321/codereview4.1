11fb96ca21f60de90eb8524b86734c8051b23951
chris berry <chrisberry321@yahoo.com>
2016-04-01 16:20:29 -0700

    Responses appending to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
-  },
+    return Ember.RSVP.hash({
+      responses: this.store.findAll('response'),
+      questions: this.store.findAll('question')
+  });
+},
   actions: {
     save3(params) {
       var newQuestion = this.store.createRecord('question', params);
